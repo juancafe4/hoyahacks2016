66c78d0e584b8b7b42601d27f70c5cd788e7dea5
Juan Carlos <juancafe2@gmail.com>
2016-11-06 01:52:40 -0500

    Added table cell

---
+++ iOS/nutriday/TableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCell.swift
+//  nutriday
+//
+//  Created by Juan Carlos Ferrel on 11/6/16.
+//  Copyright © 2016 Juan Carlos Ferrel. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
